62c0d38c95e8eedad5bb06c3d5d40ccc10246b19
Kristen Papke <klpapke215@gmail.com>
2018-07-15 02:09:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  array.find? do |cheese_types|
+  find(array) do |cheese_types|
   cheese_types = ["cheddar", "gouda", "camembert"]
   end
 end
